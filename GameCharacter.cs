a8dfe0fa75feb47db6dfe7c98538af7c87917535
coderold <mm35molina@gmail.com>
2024-09-29 21:49:53 +0800

    Add project files.

---
+++ GameCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPGame
+{
+    class GameCharacter
+    {
+        struct INFO
+        {
+            public string name;
+            public string level;
+        }
+
+        struct STATS
+        {
+            public int Strength;
+            public int Agility;
+            public int Vitality;
+            public int Intelligence;
+            public int Dexterity;
+            public int Luck;
+
+
+        }
+    }
+}
